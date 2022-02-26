df4c944a3489d6cfec2c22731791ba5257ec66cb
Done Ribeiro <www.done_@hotmail.com>
2022-02-25 21:06:37 -0300

    dynamic assignment of elements

--- src/util/format.js
+++ src/util/format.js
@@ -1,3 +1,19 @@
 class Format {
+  static getCamelCase(text) {
+    /**
+     * criar uma div so pra gerar o dataset
+     * para depois recuperarmos com o js
+    */
+    let div = document.createElement('div');
+    div.innerHTML = `<div data-${text}="id"></div>`;
+
+    /**
+     * Object.keys -> transforma o dataset em camelCase
+     * agora vamos retornar o primeiro filho da div
+     * e colocar no "id"
+    */
+    return Object.keys(div.firstChild.dataset)[0];
+    
+  }
 
 }
