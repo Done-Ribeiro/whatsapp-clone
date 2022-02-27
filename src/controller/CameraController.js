a91a07cc3ea0b84328e20bbd712989b6e3714f4c
Done Ribeiro <www.done_@hotmail.com>
2022-02-27 14:56:23 -0300

    stop camera method

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -6,10 +6,17 @@ export class CameraController {
     navigator.mediaDevices.getUserMedia({
       video: true
     }).then(stream => {
+      this._stream = stream;//* coloquei dentro do this pra ter acesso de fora
       this._videoEl.srcObject = new MediaStream(stream);//! bugfix - nova forma de obter imagem da camera
       this._videoEl.play();// força a mostrar a imagem (neste caso)
     }).catch(err => {
       console.error(err);
+    });
+  }
+
+  stop() {
+    this._stream.getTracks().forEach(track => {
+      track.stop();// para cada faixa da camera (audio, video ...)
     });
   }
 
