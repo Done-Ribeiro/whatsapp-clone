21a16d23185b067902e313bab3415254efc48dbd
Done Ribeiro <www.done_@hotmail.com>
2022-02-28 11:48:27 -0300

    pdf.js - read first page

--- src/controller/DocumentPreviewController.js
+++ src/controller/DocumentPreviewController.js
@@ -11,12 +11,12 @@ export class DocumentPreviewController {
 
   getPreviewData() {
     return new Promise((s, f) => {// s -> solved | f -> failure
+      let reader = new FileReader();
       switch (this._file.type) {
         case 'image/png':
         case 'image/jpeg':
         case 'image/jpg':
         case 'image/gif':
-          let reader = new FileReader();
           reader.onload = e => {
             s({
               src: reader.result,
@@ -29,6 +29,27 @@ export class DocumentPreviewController {
           reader.readAsDataURL(this._file);
           break;
         case 'application/pdf':
+          reader.onload = e => {
+            pdfjsLib.getDocument(new Uint8Array(reader.result)).then(pdf => {
+              /** 
+               * * agora precisamos converter os dados de arrayBuffer -> array de 8 bits
+               * ! desta forma -> new Uint8Array()
+               * ! passando o
+               * ! (reader.result) -> conteudo do nosso arquivo que foi convertido em arrayBuffer
+               * * que é o maximo que ele consegue ler em tempo real
+               * * pq se passar do jeito que está ele não consegue ler..
+               * ? a nao ser por URL de um arquivo fisico (já salvo em um servidor)
+              */
+              pdf.getPage(1).then(page => {//! agora pega a pagina 1
+                console.log('page', page);
+              }).catch(err => {
+                f(err);
+              });
+            }).catch(err => {
+              f(err);
+            });
+          }
+          reader.readAsArrayBuffer(this._file);
           break;
         default:
           f();
